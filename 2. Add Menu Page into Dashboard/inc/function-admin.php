6a24d7d2e2ea7b923c2f119d39d80af26619509e
chefboyronron <rgpseminiano@gmail.com>
2017-10-24 20:30:04 +0800

    Fixes typo error

--- 2. Add Menu Page into Dashboard/inc/function-admin.php
+++ 2. Add Menu Page into Dashboard/inc/function-admin.php
@@ -7,7 +7,7 @@
 
 function sunset_addmin_page(){
 
-	add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'ronnn-sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110);
+	add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'ronnn_sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110);
 
 }
 add_action( 'admin_menu', 'sunset_addmin_page' );
