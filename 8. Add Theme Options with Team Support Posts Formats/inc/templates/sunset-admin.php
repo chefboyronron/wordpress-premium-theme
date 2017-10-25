80e84a047c27d6afb99a40290a9535c95d3288af
chefboyronron <rgpseminiano@gmail.com>
2017-10-25 21:24:24 +0800

    Add theme options supports post formats

---
+++ 8. Add Theme Options with Team Support Posts Formats/inc/templates/sunset-admin.php
@@ -0,0 +1,26 @@
+<h1>Sunset Theme Options</h1>
+<?php settings_errors(); ?>
+<?php
+	$picture = esc_attr( get_option( 'profile_picture' ) );
+	$firstName = esc_attr( get_option( 'first_name' ) );
+	$lastName = esc_attr( get_option( 'last_name' ) );
+	$fullName = $firstName . ' ' . $lastName;
+	$description = esc_attr( get_option( 'user_description' ) );
+?>
+<div class="sunset-sidebar-preview">
+	<div class="sunset-sidebar">
+		<div class="image-container">
+			<div id="profile-picture-preview" class="profile-picture" style="background-image:url(<?php echo $picture; ?>);"></div>
+		</div>
+		<h1 class="sunset-username"><?php echo $fullName; ?></h1>
+		<h2 class="sunset-description"><?php echo $description; ?></h2>
+		<div class="icons-wrapper">
+
+		</div>
+	</div>
+</div>
+<form method="post" action="options.php" class="sunset-general-form">
+	<?php settings_fields( 'sunset-settings-group' ); ?>
+	<?php do_settings_sections( 'ronnn_sunset' ); ?>
+	<?php submit_button(); ?>
+</form>
